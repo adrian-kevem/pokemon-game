936df26d2d89fb4f1565ad4fba303e4182fbf3ab
Adrian Kevem <adrianveven@gmail.com>
2022-12-16 14:30:49 -0300

    update

--- personagens.py
+++ personagens.py
@@ -13,18 +13,30 @@ class Personagens:
         return (self.nome)
     
     def mostrar_pokemons(self):
-        for pokemon in self.pokemons:
-            print (pokemon)
+        if self.pkemons:
+            for pokemon in self.pokemons:
+                print(pokemon)
+        else:
+            print("Você ainda não possui nenhum pokemon!")
 
 
 class Jogador(Personagens):
     tipo = "jogador"
 
+    def capturar(self, pokemon):
+        self.pokemons.append(pokemon)
+        print(f"{pokemon} capturado!")
+
 class Oponente(Personagens):
-    tipo = "Opnente"
+    tipo = "Oponente"
 
 pokemon1 = PokemonEletrico("Pikachu")
 pokemon2 = PokemonFogo("Charmander")
 
 jogador1 = Jogador(nome = "Adrian", pokemons = [pokemon1, pokemon2])
+
+
+pokemon_selvagem = PokemonAgua("Blastoise")
+print(f"Um pokemon selvagem apareceu! Se trata de um {pokemon_selvagem}!")
+jogador1.capturar(pokemon_selvagem)
 jogador1.mostrar_pokemons()
